435c4658b95a073789e5ee288e7c2f5f38a91f72
alok882412 <alok.prajapati@doubtnut.com>
2025-07-08 12:57:13 +0530

    Add BPS-2.sql to create badaadata table

---
+++ liquibase/changelog/BPS-2.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE TABLE badaadata (
+    id INT,
+    name VARCHAR(100),
+    age INT,
+    salary INT
+);
+
+INSERT INTO badaadata (id, name, age, salary)
+VALUES (2, 'Karan', 28, 200);
